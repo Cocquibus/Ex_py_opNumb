12d4a4f05bff56898adf7f9133c5fc08f631de74
cocquibus <cocquibus.y@gmail.be>
2022-05-06 13:17:52 +0200

    operation rand,min,max

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+import math
+nbr = 8.66
+print(round(nbr))
+print(int(nbr))
+print(random.uniform(0,1))
+print(8**2)
+print(math.sqrt(9))
+
+tableaunbr = [1,9,3,22,0,19,2]
+
+tableaureponse = max(tableaunbr)
+print(tableaureponse)
+
+tableaureponse = min(tableaunbr)
+print(tableaureponse)
